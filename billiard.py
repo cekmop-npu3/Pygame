0db04ff91e522d3e4bf355edae69a075060a90a9
Igor Serkov <115369588+cekmop-npu3@users.noreply.github.com>
2023-12-19 17:14:43 +0300

    Add files via upload

--- billiard.py
+++ billiard.py
@@ -7,44 +7,63 @@ from itertools import combinations
 
 
 class Vector:
-    def __init__(self, end_pos: tuple[float, float], start_pos: tuple[float, float] = (.0, .0)):
+    def __init__(self, end_pos: list[float, float], start_pos: list[float, float] = (.0, .0)):
         self.start_pos = start_pos
         self.end_pos = end_pos
         self.length = dist(self.end_pos, self.start_pos)
 
     def normalize(self) -> Vector:
         return Vector(
-            end_pos=(
+            end_pos=[
                 (self.end_pos[0] - self.start_pos[0]) / self.length,
                 (self.end_pos[1] - self.start_pos[1]) / self.length
-            )
+            ]
+        )
+
+    def _add(self, other: Vector) -> Vector:
+        return Vector(
+            end_pos=[
+                self.end_pos[0] + other.end_pos[0],
+                self.end_pos[1] + other.end_pos[1]
+            ]
+        )
+
+    @staticmethod
+    def _sub(vec1: Vector, vec2: Vector) -> Vector:
+        return Vector(
+            end_pos=[
+                vec1.end_pos[0] - vec2.end_pos[0],
+                vec1.end_pos[1] - vec2.end_pos[1]
+            ]
+        )
+
+    def _mul(self, other: Union[Vector, float]) -> Union[Vector, float]:
+        return self.end_pos[0] * other.end_pos[0] + self.end_pos[1] * other.end_pos[1] if isinstance(other, Vector) else Vector(
+            end_pos=[
+                self.end_pos[0] * other, self.end_pos[1] * other
+            ]
         )
 
     def __repr__(self):
         return f'Vector({self.length=}, {self.end_pos=}, {self.start_pos=})'
 
     def __mul__(self, other: Union[Vector, float]) -> Union[Vector, float]:
-        return self.end_pos[0] * other.end_pos[0] + self.end_pos[1] * self.end_pos[1] if isinstance(other, Vector) else Vector(
-            end_pos=(
-                self.end_pos[0] * other, self.end_pos[1] * other
-            )
-        )
+        return self._mul(other)
+
+    def __rmul__(self, other: Union[Vector, float]) -> Union[Vector, float]:
+        return self._mul(other)
 
     def __sub__(self, other: Vector) -> Vector:
-        return Vector(
-            end_pos=(
-                self.end_pos[0] - self.end_pos[0],
-                self.end_pos[1] - self.end_pos[1]
-            )
-        )
+        return self._sub(self, other)
+
+    def __rsub__(self, other: Vector) -> Vector:
+        return self._sub(other, self)
 
     def __add__(self, other: Vector) -> Vector:
-        return Vector(
-            end_pos=(
-                self.end_pos[0] + self.end_pos[0],
-                self.end_pos[1] + self.end_pos[1]
-            )
-        )
+        return self._add(other)
+
+    def __radd__(self, other: Vector) -> Vector:
+        return self._add(other)
 
 
 class Ball:
@@ -62,50 +81,44 @@ class Ball:
         self.x = center[0]
         self.y = center[1]
         self.radius = radius
-        self.angle = angle
-        self.mass = 50
+        self.mass = radius
         self.color = color
         self.width = width
-        self.velocity = velocity
-        self.velocity_x = cos(radians(self.angle))
-        self.velocity_y = sin(radians(self.angle))
-        self.vector = Vector(end_pos=(self.velocity * self.velocity_x, self.velocity * self.velocity_y))
+        self.vector = Vector(end_pos=[velocity * cos(radians(angle)), velocity * sin(radians(angle))])
 
     @staticmethod
     def _calculate_velocity(ball: Ball, other: Ball) -> Vector:
-        unit_normal = Vector(start_pos=(ball.x, ball.y), end_pos=(other.x, other.y)).normalize()
-        unit_tangent = Vector(end_pos=(-unit_normal.end_pos[1], unit_normal.end_pos[0]))
-        return (unit_normal * ((unit_normal * ball.vector * (ball.mass - other.mass) + other.vector * unit_normal * 2 * other.mass) / ball.mass + other.mass) + ball.vector * (unit_tangent * unit_tangent)).normalize() * -1
+        unit_normal = Vector(start_pos=[ball.x, ball.y], end_pos=[other.x, other.y]).normalize()
+        unit_tangent = Vector(end_pos=[-unit_normal.end_pos[1], unit_normal.end_pos[0]])
+        return ((unit_normal * ball.vector * (ball.mass - other.mass) + 2 * other.mass * unit_normal * other.vector) / (ball.mass + other.mass)) * unit_normal + unit_tangent * ball.vector * unit_tangent
 
     def collision(self, other: Ball):
         if dist([self.x, self.y], [other.x, other.y]) <= self.radius + other.radius:
             vec1 = self._calculate_velocity(self, other)
             vec2 = self._calculate_velocity(other, self)
-            self.velocity, other.velocity = other.velocity, self.velocity
-            self.velocity_x, self.velocity_y = vec1.end_pos[0], vec1.end_pos[1]
-            other.velocity_x, other.velocity_y = vec2.end_pos[0], vec2.end_pos[1]
+            self.vector, other.vector = vec1, vec2
 
     def update(self):
         if self.x + self.radius > self._window.get_width() or self.x - self.radius < 0:
-            self.velocity_x *= -1
+            self.vector.end_pos[0] *= -1
         if self.y + self.radius > self._window.get_height() or self.y - self.radius < 0:
-            self.velocity_y *= -1
-        self.x += self.velocity * self.velocity_x
-        self.y += self.velocity * self.velocity_y
+            self.vector.end_pos[1] *= -1
+        self.x += self.vector.end_pos[0]
+        self.y += self.vector.end_pos[1]
         pygame.draw.circle(self._window, color=self.color, center=(self.x, self.y), radius=self.radius,
                            width=self.width)
 
     def x_ray(self):
         pygame.draw.line(self._window, color=(100, 100, 255), start_pos=(self.x, self.y), end_pos=(
-            self.x + 100 * self.velocity_x,
-            self.y + 100 * self.velocity_y),
+            self.x + 30 * self.vector.end_pos[0],
+            self.y + 30 * self.vector.end_pos[1]),
                          width=5)
         pygame.draw.line(self._window, color=(100, 255, 100), start_pos=(self.x, self.y), end_pos=(
             self.x,
-            self.y + 100 * self.velocity_y),
+            self.y + 30 * self.vector.end_pos[1]),
                          width=5)
         pygame.draw.line(self._window, color=(255, 100, 100), start_pos=(self.x, self.y), end_pos=(
-            self.x + 100 * self.velocity_x,
+            self.x + 30 * self.vector.end_pos[0],
             self.y), width=5)
 
 
@@ -115,14 +128,14 @@ class Window:
         pygame.display.set_caption(title)
         self.clock = pygame.time.Clock()
         self.balls = [
-            Ball(self.window, (500, 680), 50, 5, 45),
-            Ball(self.window, (700, 100), 50, 3, 45),
-            Ball(self.window, (100, 100), 50, 2, 0),
-            Ball(self.window, (1000, 800), 50, 5, 0),
-            Ball(self.window, (1500, 900), 50, 5, 15),
-            Ball(self.window, (1200, 800), 50, 5, 360),
+            Ball(self.window, (500, 680), 150, 2, 45),
+            Ball(self.window, (700, 100), 75, 3, 45),
+            Ball(self.window, (100, 100), 100, 2, 0),
+            Ball(self.window, (1000, 800), 25, 5, 0),
+            Ball(self.window, (1500, 900), 150, 5, 15),
+            Ball(self.window, (1200, 800), 25, 5, 360),
             Ball(self.window, (1300, 800), 50, 5, 75),
-            Ball(self.window, (1000, 900), 50, 5, 94)
+            Ball(self.window, (1000, 900), 25, 5, 94)
         ]
 
     def update(self):
