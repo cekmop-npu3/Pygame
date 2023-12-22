731d82473a97ae1d99e2640dc76ff9fe4f12440a
Igor Serkov <115369588+cekmop-npu3@users.noreply.github.com>
2023-12-22 23:37:57 +0300

    Add files via upload

--- billiard.py
+++ billiard.py
@@ -2,8 +2,9 @@ from __future__ import annotations
 import pygame
 from sys import exit
 from typing import Union
-from math import dist, radians, cos, sin, atan2, degrees
+from math import dist, radians, cos, sin, atan2, degrees, fabs
 from itertools import combinations
+from dataclasses import dataclass
 
 
 class Vector:
@@ -94,54 +95,70 @@ class Ball:
             self.vector, other.vector = vec1, vec2
 
     def update(self):
-        if self.x + self.radius > self.window.get_width() or self.x - self.radius < 0:
+        if (a := self.x + self.radius > self.window.get_width()) or self.x - self.radius < 0:
             self.vector.end_pos[0] *= -1
-        if self.y + self.radius > self.window.get_height() or self.y - self.radius < 0:
+            self.x = fabs(a * self.window.get_width() - self.radius)
+        if (b := self.y + self.radius > self.window.get_height()) or self.y - self.radius < 0:
             self.vector.end_pos[1] *= -1
+            self.y = fabs(b * self.window.get_height() - self.radius)
         self.x += self.vector.end_pos[0]
         self.y += self.vector.end_pos[1]
+
+    def draw(self):
         pygame.draw.circle(self.window, color=self.color, center=(self.x, self.y), radius=self.radius, width=self.width)
 
 
-class Window:
+@dataclass(slots=True)
+class WindowVariables:
+    balls: list[Ball]
+    pause: tuple[int, int] = (0, 1)
+    press_pos: tuple[Union[float, int], Union[float, int]] = (0, 0)
+    ball_size: int = 50
+    increment: int = 1
+
+
+class Window(WindowVariables):
     def __init__(self, title: str):
-        pygame.init()
         self.window = pygame.display.set_mode(flags=pygame.FULLSCREEN)
+        super().__init__(
+            balls=[
+                Ball(self.window, (500, 500), 50, 0, 0),
+                Ball(self.window, (800, 530), 50, 5, 180),
+                Ball(self.window, (1500, 500), 50, 0, 0),
+                Ball(self.window, (1500, 800), 50, 5, -90)
+            ]
+        )
+        pygame.init()
         pygame.display.set_caption(title)
         self.clock = pygame.time.Clock()
-        self._post_init()
-
-    def _post_init(self):
-        self.balls = [
-            Ball(self.window, (500, 500), 50, 0, 0),
-            Ball(self.window, (800, 530), 50, 5, 180),
-            Ball(self.window, (1500, 500), 50, 0, 0),
-            Ball(self.window, (1500, 800), 50, 5, -90),
-        ]
-        self.press_pos = tuple()
-        self.ball_size = 50
-        self.increment = 1
 
     def update(self):
-        [ball.update() for ball in self.balls]
-        [ball.collision(other) for ball, other in combinations(self.balls, 2)]
+        for ball in self.balls:
+            if not list(self.pause)[0]:
+                ball.update()
+            ball.draw()
+        [ball.collision(other) for ball, other in combinations(self.balls, 2) if not list(self.pause)[0]]
         self.window.blit(pygame.font.SysFont('arial', 50).render(f'Fps: {str(int(self.clock.get_fps()))}', True, (100, 100, 255)), (20, 20))
         self.window.blit(pygame.font.SysFont('arial', 50).render(f'Size: {str(self.ball_size)}', True, (100, 100, 255)), (20, 100))
+        self.window.blit(pygame.font.SysFont('arial', 50).render(f'Parts: {str(len(self.balls))}', True, (100, 100, 255)), (20, 180))
         pygame.display.update()
 
     def event_handler(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    exit()
+                if event.key == pygame.K_SPACE:
+                    self.pause = self.pause[::-1]
             if event.type == pygame.MOUSEWHEEL:
                 self.ball_size = (a if (a := self.ball_size + self.increment) < 300 else 300) if event.y > 0 else (b if (b := self.ball_size - self.increment) > 1 else 1)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     self.press_pos = pygame.mouse.get_pos()
                 elif event.button == 3:
-                    self.balls.remove(next(filter(lambda ball: dist([ball.x, ball.y], [(a := pygame.mouse.get_pos())[0], a[1]]) < ball.radius, self.balls)))
+                    [self.balls.remove(ball) for ball in filter(lambda ball: dist((ball.x, ball.y), pygame.mouse.get_pos()) < ball.radius, self.balls) if ball in self.balls]
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
                 self.balls.append(Ball(self.window, (a := pygame.mouse.get_pos()), self.ball_size, dist([self.press_pos[0], self.press_pos[1]], [a[0], a[1]]) // 20, degrees(atan2((self.press_pos[1] - a[1]), self.press_pos[0] - a[0]))))
         keys = pygame.mouse.get_pressed()
